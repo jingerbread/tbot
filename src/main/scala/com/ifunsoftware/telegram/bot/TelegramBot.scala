9da1fcc5fcf8a8c982315e22071df52032ffe525
Maria Fedorova <qwadrillion@gmail.com>
2017-03-25 19:49:51 +0300

    [added] example

--- src/main/scala/com/ifunsoftware/telegram/bot/TelegramBot.scala
+++ src/main/scala/com/ifunsoftware/telegram/bot/TelegramBot.scala
@@ -1,5 +1,11 @@
 package com.ifunsoftware.telegram.bot
 
+import spray.http.{HttpRequest, HttpResponse}
+import scala.concurrent.Future
+import spray.client.pipelining._
+
+import scala.concurrent.Future
+
 /**
   * Created by jingerbread on 25/03/2017.
   */
@@ -9,11 +15,11 @@ package com.ifunsoftware.telegram.bot
 class TelegramBot {
   val token = "331616983:AAHnWOvHqFkNCNJqVuJAx6P7KatGzYY2m7s"
 
-  def getAboutInfo():String = {
-    //val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
-    //val response: Future[HttpResponse] = pipeline(Get("http://spray.io/"))
+  def getAboutInfo:String = {
+    val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
+    val response: Future[HttpResponse] = pipeline(Get("http://spray.io/"))
 
-    ""
+    "response" + response
   }
 }
 
@@ -22,7 +28,7 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
     val tbot:TelegramBot = new TelegramBot()
-    val result = tbot.getAboutInfo
+    val result = tbot.getAboutInfo()
     println(result)
   }
 }
