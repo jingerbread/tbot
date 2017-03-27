1aff7c68c86bf2f38d84eaac7a1e1648ec1b91f4
Maria Fedorova <qwadrillion@gmail.com>
2017-03-27 09:38:56 +0300

    commit

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "tbot"
 
 version := "1.0"
 
-scalaVersion := "2.11.1"
+scalaVersion := "2.11.8"
 
 resolvers ++= List(
   "spray repo" at "http://repo.spray.io", // release builds
@@ -12,3 +12,9 @@ resolvers ++= List(
 mainClass in (Compile,run) := Some("com.ifunsoftware.telegram.bot.HelloWorld")
 
 libraryDependencies += "io.spray" %% "spray-client" % "1.3.4"
+
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.4.17"
+
+libraryDependencies += "org.apache.logging.log4j" % "log4j-api" % "2.8.1"
+
+libraryDependencies += "org.apache.logging.log4j" % "log4j-core" % "2.8.1"
--- src/main/scala/com/ifunsoftware/telegram/bot/TelegramBot.scala
+++ src/main/scala/com/ifunsoftware/telegram/bot/TelegramBot.scala
@@ -1,25 +1,34 @@
 package com.ifunsoftware.telegram.bot
 
+import akka.actor.ActorSystem
 import spray.http.{HttpRequest, HttpResponse}
+
 import scala.concurrent.Future
 import spray.client.pipelining._
+import spray.http.StatusCodes.Success
 
 import scala.concurrent.Future
-
-/**
-  * Created by jingerbread on 25/03/2017.
-  */
-
-
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.util.Failure
 
 class TelegramBot {
+  implicit val system = ActorSystem()
   val token = "331616983:AAHnWOvHqFkNCNJqVuJAx6P7KatGzYY2m7s"
 
   def getAboutInfo:String = {
     val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
-    val response: Future[HttpResponse] = pipeline(Get("http://spray.io/"))
-
-    "response" + response
+    val getMe:String = s"https://api.telegram.org/bot$token/getMe, $token"
+    println(getMe)
+    val response: Future[HttpResponse] = pipeline(Get("https://api.telegram.org/bot" + token +"/getMe"))
+    response.onComplete({
+      case Success(response) => {
+        println(response.toString)
+      }
+      case Failure(exception) => {
+        //Do something with my error
+      }
+    })
   }
 }
 
@@ -28,7 +37,7 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
     val tbot:TelegramBot = new TelegramBot()
-    val result = tbot.getAboutInfo()
+    val result = tbot.getAboutInfo
     println(result)
   }
 }
