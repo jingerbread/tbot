0f21b2821170bede0cfed9cfb0d9001fee5a33d8
Maria Fedorova <qwadrillion@gmail.com>
2017-03-27 22:33:21 +0300

    [supported] send msg

--- src/main/scala/com/ifunsoftware/telegram/bot/TelegramBot.scala
+++ src/main/scala/com/ifunsoftware/telegram/bot/TelegramBot.scala
@@ -1,11 +1,12 @@
 package com.ifunsoftware.telegram.bot
 
 import akka.actor.ActorSystem
-import spray.http.{HttpRequest, HttpResponse, MediaTypes}
+import spray.http.{HttpEntity, HttpRequest, HttpResponse, MediaTypes}
 import spray.client.pipelining._
 import spray.http.StatusCodes.Success
 import spray.httpx.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
+
 import scala.concurrent.Future
 import scala.concurrent._
 import ExecutionContext.Implicits.global
@@ -16,7 +17,9 @@ import scala.util.{Success => ScalaSuccess}
 class TelegramBot {
   implicit val system = ActorSystem()
   val token = "331616983:AAHnWOvHqFkNCNJqVuJAx6P7KatGzYY2m7s"
+  val chatId = 167389
   val API_URL = "https://api.telegram.org/bot" + token + "/"
+  val NO_RESPONSE = "NO_RESPONSE"
 
   def getAboutInfo:String = {
     val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
@@ -26,22 +29,21 @@ class TelegramBot {
     response onComplete {
       case ScalaSuccess(result) =>  {
         println(result)
-        return result.toString();
+        return result.toString
       }
 
       case Failure(error) => {
         println(error.getMessage)
-        return error.getMessage;
+        return error.getMessage
       }
     }
 
-    "NO_RESPONSE"
+    NO_RESPONSE
   }
 
-  def send_reply(response:String) = {
+  def send_reply(text:String) = {
     val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
-    val response: Future[HttpResponse] = pipeline(Post(API_URL + "sendMessage", HttpBody(MediaTypes."application/json",
-      """{"email":"foo", "password":"foo" }""")))
+    val response: Future[HttpResponse] = pipeline(Post(API_URL + "sendMessage", "{'chat_id': " + chatId +", 'text': '" + text + "'}"))
     response onComplete {
       case ScalaSuccess(result) => println(result)
 
@@ -57,5 +59,6 @@ object HelloWorld {
     val tbot:TelegramBot = new TelegramBot()
     val result = tbot.getAboutInfo
     println(result)
+    tbot.send_reply("Hello, world!")
   }
 }
